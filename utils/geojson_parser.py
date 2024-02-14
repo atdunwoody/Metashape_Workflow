fixed ypr2opk: prior disagreemnt of radians & degrees resolved

utils/geojson_parser.py
@@ -1,10 +1,11 @@
 
 import json
 import csv
+import numpy as np
 
 # Load the raw data from the file
-jfile = r"Z:\ATD\Metashape_Alignment_Tests\Only_Checking_Initial_Photos\Test\MM_102123 Flight 01.geotaglog"
-csv_fn = r"Z:\ATD\Metashape_Alignment_Tests\Only_Checking_Initial_Photos\Test\MM_102123 Flight 01.csv"
+jfile = r"Z:\ATD\Metashape_Alignment_Tests\Only_Checking_Initial_Photos\Test\MM_102123 Flight 02.geotaglog"
+csv_fn = r"Z:\ATD\Metashape_Alignment_Tests\Only_Checking_Initial_Photos\Test\MM_102123 Flight 02.csv"
 with open(jfile, 'r') as file:
     data = json.load(file)
     images = data['images']  # Access the list of images
@@ -28,9 +29,9 @@ with open(csv_fn, 'w', newline='') as csvfile:
             'longitude [decimal degrees]': coordinate[1],
             'latitude [decimal degrees]': coordinate[0],
             'altitude [meter]': coordinate[2],
-            'yaw [degrees]': geo_ref['yaw'],
-            'pitch [degrees]': geo_ref['pitch'],
-            'roll [degrees]': geo_ref['roll'],
+            'yaw [degrees]': np.degrees(geo_ref['yaw']),
+            'pitch [degrees]': np.degrees(geo_ref['pitch']),
+            'roll [degrees]': np.degrees(geo_ref['roll']),
             'accuracy horizontal [meter]': geo_ref['hAccuracy'],
             'accuracy vertical [meter]': geo_ref['vAccuracy']
         })

utils/ypr_to_opk.py
@@ -40,15 +40,16 @@ def ypr_to_opk(yaw, pitch, roll):
 
     return omega_deg, phi_deg, kappa_deg
 
-# Example usage
-yaw = -1.3782637119293213 # Yaw in degrees
-pitch = -0.027668341078037673 # Pitch in degrees
-roll = -0.069545872509479523# Roll in degrees
+ypr =[1.853051662440684,-0.14336860460805362, 0.02338354103267193]
+
+print(f"Yaw: {ypr[0]} rad, Pitch: {ypr[1]} rad, Roll: {ypr[2]} rad")
+
+yaw = np.degrees(ypr[0]) # Yaw in degrees
+pitch = np.degrees(ypr[1]) # Pitch in degrees
+roll = np.degrees(ypr[2]) # Roll in degrees
 
 omega, phi, kappa = ypr_to_opk(yaw, pitch, roll)
-print(f"Omega: {omega}, Phi: {phi}, Kappa: {kappa}")
-#Convert Omega Phi Kappa to degrees
-omega = np.degrees(omega)
-phi = np.degrees(phi)
-kappa = np.degrees(kappa)
-print(f"Omega: {omega}, Phi: {phi}, Kappa: {kappa}")
+
+#print with formatted degree symbol
+print(f"Omega: {omega}°, Phi: {phi}°, Kappa: {kappa}°")
+